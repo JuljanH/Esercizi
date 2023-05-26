288a63b8d9a4928e418315cb9b377d6e8efce171
JuljanH <juljan.hoxha@alten.it>
2023-05-26 17:34:48 +0200

    Add Esercizio MVVM

---
+++ FileCounter_MVVM/Model/FileCounter_M.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using static FileCounter_MVVM.Model.IFileCounter_M;
+
+namespace FileCounter_MVVM.Model
+{
+    public class FileCounter_M : IFilterCounter_M
+    {
+        public string PathString { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return Directory.GetFiles(PathString, "*", SearchOption.AllDirectories).Length;
+            }
+            set
+            {
+                Directory.Delete(PathString, true);
+            }
+        }
+
+    }
+}
---
+++ FileCounter_MVVM/ViewModel/FileCounterVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace FileCounter_MVVM.ViewModel
+{
+    public class FileCounterVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string FolderPath)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(FolderPath));
+        }
+    }
+}
