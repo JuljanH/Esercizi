fb3cd969267a79fd742cb0bfd86bd73d84d1b985
JuljanH <juljan.hoxha@alten.it>
2023-05-30 17:13:54 +0200

    Add new project

---
+++ Contatore/Model/IContatore_M.cs
@@ -0,0 +1,11 @@
+﻿namespace Contatore.Model
+{
+    internal class IContatore_M
+    {
+        public interface IContatore_M1
+        {
+            string Path { get; set; }
+            int Conteggio { get; }
+        }
+    }
+}
---
+++ Contatore/ViewModel/ContatoreVM.cs
@@ -0,0 +1,47 @@
+﻿using Contatore.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Contatore.Model.IContatore_M;
+
+namespace Contatore.ViewModel
+{
+    internal class ContatoreVM : INotifyPropertyChanged
+    {
+        private ModelContatore model;
+        public ContatoreVM()
+        {
+            model = new ModelContatore();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string variablename) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(variablename));
+        }
+
+        public string Path
+        {
+            get { return model.Path; }
+            set
+            {
+                OnPropertyChanged(nameof(bgColor));
+                OnPropertyChanged(nameof(Path));
+                OnPropertyChanged(nameof(Conteggio));
+            }
+        }
+
+        public string bgColor()
+        {
+            get { return model.Conteggio; }
+
+        }
+
+        public int Conteggio
+        {
+            get { return model.Conteggio; }
+        }
+    }
+}
